82950473f0cf3b7f4dcf8f56e8604a822a682df3
Thiago Cardoso <cafeprathi@gmail.com>
2024-08-28 17:30:33 -0300

    serializer validation

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from movies.models import Movie
+from datetime import date
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('The release data must be bigger then 1970.')
+        elif value.year > (date.today().year + 1):
+            print(date.today().year+1)
+            raise  serializers.ValidationError(f'The release must be iqual or lower than {date.today().year} ')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 1000:
+            raise serializers.ValidationError('Resume must have max 1000 caracters.')
